83f1b70ac7a7d6ad04f9c069e5eeeb91120b6055
Andrew <marianoandres191200@gmail.com>
2022-02-03 22:32:01 -0300

    firebase testing button to upload cards  to database - loading text is now visible when cards are loading, if there isn´t any cards in the database, ´No cards´ sign will be shown

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/card.jsx';
 
-import { getFirestore, collection, getDocs, connectFirestoreEmulator } from "firebase/firestore";
+import { getFirestore, collection, getDocs, doc, setDoc, addDoc, query } from "firebase/firestore";
 import { db } from './firebase';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,30 +10,46 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
+  const [emptyCardCollection, setEmptyCardCollection] = useState(false)
 
-  useEffect(async () => {
+  useEffect(async () => { //READ DATA FROM DATABASE WHEN THE PAGE LOADS
     const querySnapshot = await getDocs(collection(db, "ToDos")); //DATABASE INSTANCE AND NAME OF THE COLLECTION
     var todos = []
     querySnapshot.forEach((doc) => {
       todos.push({
         id: doc.id, ...doc.data()
       });
+      //querySnapshot.size RETURNS NUMBER OF DOCUMENTS IN A COLLECTION
     });
+    if(todos.length === 0) setEmptyCardCollection(true);
+    else setEmptyCardCollection(false);
     setCards(todos);
-  }, [cards])
+  }, [cards]);
 
+  async function addCardToDatabase() {
+    await addDoc(collection(db, 'ToDos'), {
+      id: cards.length + 1,
+      title: `card ${cards.length + 1} title`,
+      content: 'content example'
+    })
+  }
 
-
+  const loadingCards = () => {
+    if(emptyCardCollection) return 'No cards';
+    if(document.getElementById('card') != null) return '';
+    else return 'Loading...';
+  }
 
   return (
     <div className="App">
+      <button id='buton' onClick={addCardToDatabase}>UPLOAD</button>
       <header>
         <h1>Todo App</h1>
       </header>
 
       <div className="loading">
-          <h1>{!document.contains(document.getElementById('card')) && 'Loading...'}</h1>
+        <h1>{loadingCards()}</h1>
       </div>
 
       <div className="todo-cards">
