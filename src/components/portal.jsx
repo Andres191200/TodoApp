0cdb29ef6e2232c177213b1e7672befb5d4a46f2
Andrew <marianoandres191200@gmail.com>
2022-02-09 14:48:06 -0300

    new card popup is now responsive

--- src/components/portal.jsx
+++ src/components/portal.jsx
@@ -8,8 +8,10 @@ const Portal = ({ trigger }) => {
                     <label for="card-title">Title</label>
                     <input type="text" name="card-title"></input>
                     <label for="card-content">Content</label>
-                    <textarea className="card-content" name="card-content" rows="10"></textarea> 
-                    <button type="submit" className="submit-new-card-btn">Create</button>
+                    <textarea className="card-content" name="card-content"></textarea>
+                    <div className="submit-btn-container">
+                        <button type="submit" className="submit-new-card-btn">Create</button>
+                    </div>
                 </form>
             </div>
         </div>
